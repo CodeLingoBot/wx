1a641d76ed2a0230aea41bd54c8329459f632572
morya <moryaden@qq.com>
2018-01-03 20:46:25 +0800

    delete comment

--- types.go
+++ types.go
@@ -15,7 +15,6 @@ type WxAutoMsg struct {
 	MsgId        string `xml:"MsgId"`
 }
 
-// "{\"curr\": 2546.2158, \"god\": 2600, \"step\": 140, \"action\": null, \"diff\": -54.784200000000055, \"now\": \"2018-01-03 19:19:16\"}"
 type StoreMsg struct {
 	Curr   float64 `json:"curr"`
 	God    float64 `json:"god"`
